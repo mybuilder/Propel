0b3ef4a6b61305871d229afb0de493f3eab188bc
Edd Mann <the@eddmann.com>
2021-01-11 12:12:23 +0000

    Correct ordering of `implode`

--- runtime/lib/query/Join.php
+++ runtime/lib/query/Join.php
@@ -521,7 +521,7 @@ class Join
             for ($i=0; $i < $this->count; $i++) {
                 $conditions []= $this->getLeftColumn($i) . $this->getOperator($i) . $this->getRightColumn($i);
             }
-            $joinCondition = sprintf('(%s)', implode($conditions, ' AND '));
+            $joinCondition = sprintf('(%s)', implode(' AND ', $conditions));
         } else {
             $joinCondition = '';
             $this->getJoinCondition()->appendPsTo($joinCondition, $params);
